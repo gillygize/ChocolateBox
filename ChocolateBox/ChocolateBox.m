4ebbfc900d76b5d92c7dd7f9ec4419c71f4dc82e
Matthew Gillingham <matthew.gillingham@gree.co.jp>
2012-01-11 11:18:34 +0900

    Initial Commit

---
+++ ChocolateBox/ChocolateBox.m
@@ -0,0 +1,23 @@
+//
+//  ChocolateBox.m
+//  ChocolateBox
+//
+//  Created by matthew.gillingham on 1/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ChocolateBox.h"
+
+@implementation ChocolateBox
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
