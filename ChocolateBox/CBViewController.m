dfde33bf0f84e854778859d45d0913fb07f7fc3e
Matthew Gillingham <gillygize@gmail.com>
2012-05-27 16:47:09 +0900

    Removed forward invocation since it is slower

--- ChocolateBox/CBViewController.m
+++ ChocolateBox/CBViewController.m
@@ -30,23 +30,80 @@
   [super dealloc];
 }
 
-- (void)forwardInvocation:(NSInvocation *)invocation
-{
-  if ([machine respondsToSelector:[invocation selector]]) {
-    [invocation invokeWithTarget:machine];
-  } else {
-    [super forwardInvocation:invocation];
-  }
+- (id<ChocolateBoxProtocol>)supermachine {
+  return [machine supermachine];
 }
 
-- (NSMethodSignature*)methodSignatureForSelector:(SEL)selector
-{
-  NSMethodSignature* signature = [super methodSignatureForSelector:selector];
-  if (!signature) {
-    signature = [machine methodSignatureForSelector:selector];
-  }
-    
-  return signature;
+- (void)setSupermachine:(id<ChocolateBoxProtocol>)supermachine {
+  [machine setSupermachine:supermachine];
+}
+
+- (id<ChocolateBoxProtocol>)submachineWithIdentifier:(id)identifier {
+  return [machine submachineWithIdentifier:identifier];
+}
+
+- (BOOL)addSubmachine:(id<ChocolateBoxProtocol>)submachine {
+  return [machine addSubmachine:submachine];
+}
+
+- (void)removeFromSupermachine {
+  [machine removeFromSupermachine];
+}
+
+- (BOOL)containsSubmachineWithIdentifier:(NSString*)identifier {
+  return ([machine submachineWithIdentifier:identifier] != nil);
+}
+
+- (BOOL)containsSubmachine:(id<ChocolateBoxProtocol>)submachine {
+  return [machine containsSubmachine:submachine];
+}
+
+- (void)removeSubmachine:(id<ChocolateBoxProtocol>)submachine {  
+  [machine removeSubmachine:submachine];
+}
+
+- (NSSet *)states {
+  return [machine states];
+}
+
+- (void)setInitialState:(NSString *)initialState {
+  [machine setInitialState:initialState];
+}
+
+- (BOOL)hasState:(NSString *)state {
+  return [machine hasState:state];;
+}
+
+- (void)addState:(NSString *)state enter:(void(^)(void))enter exit:(void(^)(NSString *previousState))exit {
+  [machine addState:state enter:enter exit:exit];
+}
+
+- (void)replaceEnter:(void(^)(void))enter exit:(void(^)(NSString *previousState))exit forState:(NSString *)state {
+  [machine replaceEnter:enter exit:exit forState:state];
+}
+
+- (void)removeState:(NSString *)state {
+  [machine removeState:state];
+}
+
+- (void)enterInitialState {
+  [machine enterInitialState];
+}
+
+- (void)transitionToState:(NSString *)state {
+  [machine transitionToState:state];
+}
+
+- (void)revalidateTransitionFromState:(NSString *)fromState toState:(NSString *)toState {
+  [machine revalidateTransitionFromState:fromState toState:toState];
+}
+
+- (void)invalidateTransitionFromState:(NSString *)fromState toState:(NSString *)toState {
+  [machine invalidateTransitionFromState:fromState toState:toState];
+}
+
+- (void)transitionToState:(NSString *)state animated:(BOOL)animated duration:(NSTimeInterval)duration {
+  [machine transitionToState:state animated:animated duration:duration];
 }
 
 @end
